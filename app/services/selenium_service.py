45cf9da1c8616d491d41032aafb0a80617e71f72
03kaan07 <kaan.cetin2001@gmail.com>
2025-03-30 22:47:58 +0200

    applied stealth to selenium

--- app/services/selenium_service.py
+++ app/services/selenium_service.py
@@ -1,21 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium_stealth import stealth
 
 class SeleniumService:
     def __init__(self):
         options = Options()
-        options.add_argument("--headless")
+        options.add_argument("--headless")  # Run in headless mode
         options.add_argument("--disable-gpu")
+        options.add_argument("start-maximized")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
+
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=options)
         self.wait = WebDriverWait(self.driver, 10)
 
+        # Apply Selenium Stealth
+        stealth(self.driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True)
+
     def close(self):
         """Close the WebDriver."""
         self.driver.quit()
-
-    
